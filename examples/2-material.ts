4255028df690788c26da90d831362512b9ede3dd
Don McCurdy <don.r.mccurdy@gmail.com>
2021-07-17 23:14:20 -0700

    Improve material example.

--- examples/2-material.ts
+++ examples/2-material.ts
@@ -1,4 +1,4 @@
-import { ACESFilmicToneMapping, AmbientLight, DirectionalLight, PMREMGenerator, PerspectiveCamera, Scene, UnsignedByteType, WebGLRenderer, sRGBEncoding } from 'three';
+import { ACESFilmicToneMapping, AmbientLight, DirectionalLight, PMREMGenerator, PerspectiveCamera, Scene, UnsignedByteType, WebGLRenderer, sRGBEncoding, TorusKnotBufferGeometry } from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import { RGBELoader } from 'three/examples/jsm/loaders/RGBELoader.js';
 import { Document, Material } from '@gltf-transform/core';
@@ -27,7 +27,7 @@ light2.position.set(1, 2, 3);
 scene.add(light1, light2);
 
 const camera = new PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.25, 20);
-camera.position.set(-1.8, 0.6, 2.7);
+camera.position.set(-4, 1.2, 5.4);
 camera.lookAt(scene.position);
 
 const controls = new OrbitControls(camera, renderer.domElement);
@@ -59,26 +59,19 @@ let material: Material;
 const doc = (() => {
 	const doc = new Document();
 	material = doc.createMaterial('Material');
+	const primTemplate = new TorusKnotBufferGeometry(1, 0.4, 100, 16);
+	const indicesArray = primTemplate.index.array as Uint16Array;
+	const positionArray = primTemplate.attributes.position.array as Float32Array;
+	const normalArray = primTemplate.attributes.normal.array as Float32Array;
+	const texcoordArray = primTemplate.attributes.uv.array as Float32Array;
 	const prim = doc.createPrimitive()
-		.setAttribute('POSITION', doc.createAccessor('p').setType('VEC3').setArray(new Float32Array([
-			0, 0, 0,
-			1, 0, 0,
-			0, 1, 0,
-			1, 1, 0,
-			0, 1, 0,
-			1, 0, 0,
-		])))
-		.setAttribute('NORMAL', doc.createAccessor('p').setType('VEC3').setArray(new Float32Array([
-			0, 0, 1,
-			0, 0, 1,
-			0, 0, 1,
-			0, 0, 1,
-			0, 0, 1,
-			0, 0, 1,
-		])))
+		.setIndices(doc.createAccessor('indices').setType('SCALAR').setArray(indicesArray))
+		.setAttribute('POSITION', doc.createAccessor('p').setType('VEC3').setArray(positionArray))
+		.setAttribute('NORMAL', doc.createAccessor('n').setType('VEC3').setArray(normalArray))
+		.setAttribute('TEXCOORD_0', doc.createAccessor('t').setType('VEC2').setArray(texcoordArray))
 		.setMaterial(material);
 	const mesh = doc.createMesh().addPrimitive(prim);
-	const node = doc.createNode().setMesh(mesh).setTranslation([-0.5, -0.5, 0]);
+	const node = doc.createNode().setMesh(mesh);
 	doc.createScene().addChild(node);
 	return doc;
 })();
@@ -93,6 +86,10 @@ const pane = createMaterialPane(doc, material);
 
 let needsUpdate = false;
 pane.on('change', () => (needsUpdate = true));
+
+let stats = {info: ''};
+const monitorFolder = pane.addFolder({index: 0, title: 'Monitor'})
+monitorFolder.addMonitor(stats, 'info', {bufferSize: 1, multiline: true, lineCount: 3});
 
 //
 
@@ -111,6 +108,13 @@ function animate() {
 	}
 
 	render();
+
+	const info = renderer.info;
+	stats.info = `
+programs     ${info.programs.length}
+geometries   ${info.memory.geometries}
+textures     ${info.memory.textures}
+	`.trim();
 }
 
 function render() {
