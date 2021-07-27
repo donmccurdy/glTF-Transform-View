11cb2ec4145d822cff7fbdbb2856e40ffe07d7ec
Don McCurdy <don.r.mccurdy@gmail.com>
2021-07-26 22:03:46 -0700

    Add 'diff' example.

---
+++ examples/3-diff.ts
@@ -0,0 +1,154 @@
+import { ACESFilmicToneMapping, AmbientLight, DirectionalLight, PMREMGenerator, PerspectiveCamera, Scene, UnsignedByteType, WebGLRenderer, sRGBEncoding, Object3D, Mesh, Material, Box3, Vector3 } from 'three';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import { RGBELoader } from 'three/examples/jsm/loaders/RGBELoader.js';
+import { Document, WebIO } from '@gltf-transform/core';
+import { metalRough } from '@gltf-transform/functions';
+import { DocumentRenderer } from '../dist/render.modern.js';
+import { ALL_EXTENSIONS } from '@gltf-transform/extensions';
+
+const renderer = new WebGLRenderer({antialias: true});
+renderer.setPixelRatio( window.devicePixelRatio );
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.outputEncoding = sRGBEncoding;
+renderer.physicallyCorrectLights = true;
+renderer.toneMapping = ACESFilmicToneMapping;
+renderer.toneMappingExposure = 1;
+
+const containerEl = document.querySelector('#container');
+containerEl.appendChild(renderer.domElement);
+
+const io = new WebIO().registerExtensions(ALL_EXTENSIONS);
+const loader = new GLTFLoader();
+
+const pmremGenerator = new PMREMGenerator(renderer);
+pmremGenerator.compileEquirectangularShader();
+
+const scene = new Scene();
+let documentRenderer: DocumentRenderer;
+let modelBefore: Object3D;
+let modelAfter: Object3D;
+
+const light1 = new AmbientLight();
+const light2 = new DirectionalLight();
+light2.position.set(1, 2, 3);
+scene.add(light1, light2);
+
+const camera = new PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.25, 20);
+camera.position.set(-1.8, 0.6, 2.7);
+camera.lookAt(scene.position);
+
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.addEventListener('change', render);
+controls.update();
+
+window.addEventListener( 'resize', onWindowResize );
+
+//
+
+new RGBELoader()
+	.setDataType( UnsignedByteType )
+	.load( '../assets/royal_esplanade_1k.hdr', ( texture ) => {
+		const envMap = pmremGenerator.fromEquirectangular( texture ).texture;
+		scene.background = envMap;
+		scene.environment = envMap;
+
+		render();
+		texture.dispose();
+		pmremGenerator.dispose();
+	} );
+
+//
+
+document.body.addEventListener('gltf-document', async (event) => {
+	const doc = (event as CustomEvent).detail as Document;
+
+	if (documentRenderer) documentRenderer.dispose();
+	if (modelBefore) dispose(modelBefore);
+	if (modelAfter) dispose(modelAfter);
+
+	await checkExtensions(doc);
+
+	console.time('DocumentRenderer::init');
+	documentRenderer = new DocumentRenderer(doc);
+	modelAfter = documentRenderer.toObject3D();
+	console.timeEnd('DocumentRenderer::init');
+
+	console.time('WebIO::writeBinary');
+	const glb = io.writeBinary(doc);
+	console.timeEnd('WebIO::writeBinary');
+
+	console.time('GLTFLoader::parse');
+	modelBefore = await new Promise((resolve, reject) => {
+		loader.parse(glb, '', ({scene}) => resolve(scene), reject);
+	}) as Object3D;
+	console.timeEnd('GLTFLoader::parse');
+
+	frameContent(modelBefore, -1);
+	frameContent(modelAfter, 1);
+
+	controls.update();
+	render();
+});
+
+//
+
+function render() {
+	renderer.render(scene, camera);
+}
+
+function onWindowResize() {
+	camera.aspect = window.innerWidth / window.innerHeight;
+	camera.updateProjectionMatrix();
+	renderer.setSize(window.innerWidth, window.innerHeight);
+	render();
+}
+
+function dispose(model: Object3D) {
+	scene.remove(model);
+	model.traverse((o) => {
+		if ((o as Mesh).isMesh) {
+			(o as Mesh).geometry.dispose();
+			((o as Mesh).material as Material).dispose();
+			// TODO(bug): Should dispose textures too, if this were production.
+		}
+	});
+}
+
+function frameContent(object: Object3D, offset: 1 | -1) {
+	const box = new Box3().setFromObject(object);
+	const size = box.getSize(new Vector3());
+	const length = box.getSize(new Vector3()).length();
+	const center = box.getCenter(new Vector3());
+
+	controls.reset();
+
+	object.position.x += (object.position.x - center.x) + offset * size.x / 2;
+	object.position.y += (object.position.y - center.y);
+	object.position.z += (object.position.z - center.z);
+	controls.maxDistance = length * 10;
+	camera.near = length / 100;
+	camera.far = length * 100;
+	camera.updateProjectionMatrix();
+
+	camera.position.copy(center);
+	camera.position.x += length / 2.0;
+	camera.position.y += length / 5.0;
+	camera.position.z += length / 2.0;
+	camera.lookAt(center);
+
+	controls.saveState();
+
+	scene.add(object);
+}
+
+async function checkExtensions(document: Document) {
+	const extensions = document.getRoot()
+		.listExtensionsUsed()
+		.map((ext) => ext.extensionName);
+	console.debug(`EXTENSIONS: ${extensions.join() || 'None'}`);
+
+	if (extensions.includes('KHR_materials_pbrSpecularGlossiness')) {
+		await document.transform(metalRough());
+	}
+}
---
+++ examples/dropzone.ts
@@ -0,0 +1,96 @@
+import { Document, JSONDocument, WebIO } from '@gltf-transform/core';
+import { ALL_EXTENSIONS } from '@gltf-transform/extensions';
+import { SimpleDropzone } from 'simple-dropzone';
+
+/**
+ * Dropzone
+ *
+ * Utility file to load glTF/GLB models and emit a Document.
+ */
+
+const io = new WebIO().registerExtensions(ALL_EXTENSIONS);
+const dropEl = document.querySelector('body');
+const placeholderEl = document.querySelector<HTMLElement>('.dropzone-placeholder');
+const inputEl = document.querySelector('#file-input');
+
+document.addEventListener('DOMContentLoaded', () => {
+	const dropzone = new SimpleDropzone(dropEl, inputEl) as any;
+	dropzone.on('drop', async ({files}) => {
+		try {
+			await load(files);
+            placeholderEl.style.display = 'none';
+		} catch (e) {
+			alert(e.message);
+		}
+	});
+});
+
+async function load (fileMap: Map<string, File>) {
+	let rootFile: File;
+	let rootPath: string;
+	let images: File[] = [];
+	Array.from(fileMap).forEach(([path, file]) => {
+		if (file.name.match(/\.(gltf|glb)$/)) {
+			rootFile = file;
+			rootPath = path.replace(file.name, '');
+		} else if (file.name.match(/\.(png|jpg|jpeg|ktx2|webp)$/)) {
+			images.push(file);
+		}
+	});
+
+	if (rootFile) return loadDocument(fileMap, rootFile, rootPath);
+	throw new Error('No .gltf, .glb, or texture asset found.');
+}
+
+async function loadDocument(fileMap: Map<string, File>, rootFile: File, rootPath: string) {
+	let jsonDocument: JSONDocument;
+	let doc: Document;
+
+	if (rootFile.name.match(/\.(glb)$/)) {
+		const arrayBuffer = await rootFile.arrayBuffer();
+		jsonDocument = io.binaryToJSON(arrayBuffer);
+	} else {
+		jsonDocument = {
+			json: JSON.parse(await rootFile.text()),
+			resources: {},
+		};
+		for (const [fileName, file] of fileMap.entries()) {
+			const path = fileName.replace(rootPath, '');
+			jsonDocument.resources[path] = await file.arrayBuffer();
+		}
+	}
+
+	normalizeURIs(jsonDocument);
+	doc = io.readJSON(jsonDocument);
+	removeCompression(doc);
+    document.body.dispatchEvent(new CustomEvent('gltf-document', {detail: doc}));
+}
+
+/**
+ * Normalize URIs to match expected output from simple-dropzone, for folders or
+ * ZIP archives. URIs in a glTF file may be escaped, or not. Assume that assetMap is
+ * from an un-escaped source, and decode all URIs before lookups.
+ * See: https://github.com/donmccurdy/three-gltf-viewer/issues/146
+ */
+function normalizeURIs (jsonDocument: JSONDocument) {
+	const images = jsonDocument.json.images || [];
+	const buffers = jsonDocument.json.buffers || [];
+	for (const resource of [...images, ...buffers]) {
+		if (!resource.uri) continue;
+		if (resource.uri.startsWith('data:')) continue;
+
+		resource.uri = decodeURI(resource.uri).replace(/^(\.?\/)/, '');
+		if (!(resource.uri in jsonDocument.resources)) {
+			throw new Error(`Missing resource: ${resource.uri}`);
+		}
+	}
+}
+
+/** Remove compression extensions now so further writes don't recompress. */
+function removeCompression(document: Document) {
+	for (const extensionName of ['KHR_draco_mesh_compression', 'EXT_meshopt_compression']) {
+		const extension = document.getRoot().listExtensionsUsed()
+			.find((extension) => extension.extensionName === extensionName);
+		if (extension) extension.dispose();
+	}
+}
