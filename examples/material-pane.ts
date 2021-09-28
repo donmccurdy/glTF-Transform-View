d613615a1d56c5ea07ecb59f010c05199486275c
Don McCurdy <don.r.mccurdy@gmail.com>
2021-09-27 21:34:40 -0700

    Add separators to material pane.

--- examples/material-pane.ts
+++ examples/material-pane.ts
@@ -108,24 +108,29 @@ export function createMaterialPane(_pane: Pane, document: Document, material: Ma
 		.on('change', () => material.setBaseColorHex(params.baseColorFactor));
 	coreFolder.addInput(params, 'baseColorTexture', {view: 'thumbnail-list', options: textureOptions})
 		.on('change', (ev) => material.setBaseColorTexture(textureFromEvent(ev)));
+	coreFolder.addSeparator();
 	coreFolder.addInput(params, 'alpha', {min: 0, max: 1})
 		.on('change', () => material.setAlpha(params.alpha));
 	coreFolder.addInput(params, 'alphaMode', {options: {OPAQUE: 'OPAQUE', BLEND: 'BLEND', MASK: 'MASK'}})
 		.on('change', () => material.setAlphaMode(params.alphaMode));
+	coreFolder.addSeparator();
 	coreFolder.addInput(params, 'emissiveFactor', {view: 'color'})
 		.on('change', () => material.setEmissiveHex(params.emissiveFactor));
 	coreFolder.addInput(params, 'emissiveTexture', {view: 'thumbnail-list', options: textureOptions})
 		.on('change', (ev) => material.setEmissiveTexture(textureFromEvent(ev)));
+	coreFolder.addSeparator();
 	coreFolder.addInput(params, 'metallicFactor', {min: 0, max: 1})
 		.on('change', () => material.setMetallicFactor(params.metallicFactor));
 	coreFolder.addInput(params, 'roughnessFactor', {min: 0, max: 1})
 		.on('change', () => material.setRoughnessFactor(params.roughnessFactor));
 	coreFolder.addInput(params, 'metallicRoughnessTexture', {view: 'thumbnail-list', options: textureOptions})
 		.on('change', (ev) => material.setMetallicRoughnessTexture(textureFromEvent(ev)));
+	coreFolder.addSeparator();
 	coreFolder.addInput(params, 'occlusionStrength', {min: 0, max: 1})
 		.on('change', () => material.setOcclusionStrength(params.occlusionStrength));
 	coreFolder.addInput(params, 'occlusionTexture', {view: 'thumbnail-list', options: textureOptions})
 		.on('change', (ev) => material.setOcclusionTexture(textureFromEvent(ev)));
+	coreFolder.addSeparator();
 	coreFolder.addInput(params, 'normalScale')
 		.on('change', () => material.setNormalScale(params.normalScale));
 	coreFolder.addInput(params, 'normalTexture', {view: 'thumbnail-list', options: textureOptions})
@@ -133,12 +138,15 @@ export function createMaterialPane(_pane: Pane, document: Document, material: Ma
 
 	const clearcoatFolder = pane.addFolder({title: 'KHR_materials_clearcoat', expanded: false});
 	clearcoatFolder.addInput(params, 'clearcoatEnabled');
+	clearcoatFolder.addSeparator();
 	clearcoatFolder.addInput(params, 'clearcoatFactor', {min: 0, max: 1});
 	clearcoatFolder.addInput(params, 'clearcoatTexture', {view: 'thumbnail-list', options: textureOptions})
 		.on('change', (ev) => clearcoat.setClearcoatTexture(textureFromEvent(ev)));
+	clearcoatFolder.addSeparator();
 	clearcoatFolder.addInput(params, 'clearcoatRoughnessFactor', {min: 0, max: 1});
 	clearcoatFolder.addInput(params, 'clearcoatRoughnessTexture', {view: 'thumbnail-list', options: textureOptions})
 		.on('change', (ev) => clearcoat.setClearcoatRoughnessTexture(textureFromEvent(ev)));
+	clearcoatFolder.addSeparator();
 	clearcoatFolder.addInput(params, 'clearcoatNormalScale');
 	clearcoatFolder.addInput(params, 'clearcoatNormalTexture', {view: 'thumbnail-list', options: textureOptions})
 		.on('change', (ev) => clearcoat.setClearcoatNormalTexture(textureFromEvent(ev)));
@@ -160,9 +168,11 @@ export function createMaterialPane(_pane: Pane, document: Document, material: Ma
 
 	const sheenFolder = pane.addFolder({title: 'KHR_materials_sheen', expanded: false});
 	sheenFolder.addInput(params, 'sheenEnabled');
+	sheenFolder.addSeparator();
 	sheenFolder.addInput(params, 'sheenColorFactor', {view: 'color'});
 	sheenFolder.addInput(params, 'sheenColorTexture', {view: 'thumbnail-list', options: textureOptions})
 		.on('change', (ev) => sheen.setSheenColorTexture(textureFromEvent(ev)));
+	sheenFolder.addSeparator();
 	sheenFolder.addInput(params, 'sheenRoughnessFactor', {min: 0, max: 1});
 	sheenFolder.addInput(params, 'sheenRoughnessTexture', {view: 'thumbnail-list', options: textureOptions})
 		.on('change', (ev) => sheen.setSheenRoughnessTexture(textureFromEvent(ev)));
@@ -175,9 +185,11 @@ export function createMaterialPane(_pane: Pane, document: Document, material: Ma
 
 	const specularFolder = pane.addFolder({title: 'KHR_materials_specular', expanded: false});
 	specularFolder.addInput(params, 'specularEnabled');
+	specularFolder.addSeparator();
 	specularFolder.addInput(params, 'specularFactor', {min: 0, max: 1});
 	specularFolder.addInput(params, 'specularTexture', {view: 'thumbnail-list', options: textureOptions})
 		.on('change', (ev) => specular.setSpecularTexture(textureFromEvent(ev)));
+	specularFolder.addSeparator();
 	specularFolder.addInput(params, 'specularColorFactor', {view: 'color'});
 	specularFolder.addInput(params, 'specularColorTexture', {view: 'thumbnail-list', options: textureOptions})
 		.on('change', (ev) => specular.setSpecularColorTexture(textureFromEvent(ev)));
@@ -190,6 +202,7 @@ export function createMaterialPane(_pane: Pane, document: Document, material: Ma
 
 	const transmissionFolder = pane.addFolder({title: 'KHR_materials_transmission', expanded: false});
 	transmissionFolder.addInput(params, 'transmissionEnabled');
+	transmissionFolder.addSeparator();
 	transmissionFolder.addInput(params, 'transmissionFactor', {min: 0, max: 1});
 	transmissionFolder.addInput(params, 'transmissionTexture', {view: 'thumbnail-list', options: textureOptions})
 		.on('change', (ev) => transmission.setTransmissionTexture(textureFromEvent(ev)));
@@ -200,9 +213,11 @@ export function createMaterialPane(_pane: Pane, document: Document, material: Ma
 
 	const volumeFolder = pane.addFolder({title: 'KHR_materials_volume', expanded: false});
 	volumeFolder.addInput(params, 'volumeEnabled');
+	volumeFolder.addSeparator();
 	volumeFolder.addInput(params, 'thicknessFactor', {min: 0, max: 1});
 	volumeFolder.addInput(params, 'thicknessTexture', {view: 'thumbnail-list', options: textureOptions})
 		.on('change', (ev) => volume.setThicknessTexture(textureFromEvent(ev)));
+	volumeFolder.addSeparator();
 	volumeFolder.addInput(params, 'attenuationColorFactor', {view: 'color'});
 	volumeFolder.addInput(params, 'attenuationDistance', {min: 0, max: 5, step: 0.01});
 	volumeFolder.on('change', () => {
