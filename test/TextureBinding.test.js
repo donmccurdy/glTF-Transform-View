bcb72d8c6f2f3123f3fd35247e6c19413e945492
Don McCurdy <dm@donmccurdy.com>
2022-04-02 18:47:11 -0400

    Tests on TextureBinding

--- test/TextureBinding.test.js
+++ test/TextureBinding.test.js
@@ -1,16 +1,53 @@
 import test from 'tape';
 import { Document } from '@gltf-transform/core';
 import { DocumentView } from '../dist/view.modern.js';
+import { LinearEncoding, sRGBEncoding } from 'three';
 
 test('TextureBinding', t => {
 	const document = new Document();
-	const textureDef = document.createTexture()
+	const textureDef = document.createTexture('MyTexture')
 		.setImage(new Uint8Array(0))
 		.setMimeType('image/png')
-		.setExtension({textureExtras: true});
+		.setExtras({textureExtras: true});
+	const materialDef = document.createMaterial()
+		.setBaseColorTexture(textureDef)
+		.setMetallicRoughnessTexture(textureDef);
+
 	const documentView = new DocumentView(document);
 	const texture = documentView.view(textureDef);
+	const material = documentView.view(materialDef);
+	const {map, metalnessMap, roughnessMap} = material;
+
 	t.ok(texture, 'texture');
-	t.notOk(texture.flipY, 'texture → flipY');
+	t.equals(map.encoding, sRGBEncoding, 'sRGB');
+	t.equals(roughnessMap.encoding, LinearEncoding, 'Linear-sRGB');
+	t.equals(metalnessMap.encoding, LinearEncoding, 'Linear-sRGB');
+	t.ok(metalnessMap === roughnessMap, 'metalnessMap === roughnessMap');
+	t.notOk(texture.flipY || map.flipY || roughnessMap.flipY || metalnessMap.flipY, 'flipY=false');
+
+	const disposed = new Set();
+	texture.addEventListener('dispose', () => disposed.add(texture));
+	map.addEventListener('dispose', () => disposed.add(map));
+	metalnessMap.addEventListener('dispose', () => disposed.add(metalnessMap));
+	roughnessMap.addEventListener('dispose', () => disposed.add(roughnessMap));
+
+	materialDef.setBaseColorTexture(null);
+	documentView.gc();
+
+	t.equals(disposed.size, 1, 'dispose count (1/3)');
+	t.ok(disposed.has(map), 'dispose map');
+
+	materialDef.dispose();
+	documentView.gc();
+
+	t.equals(disposed.size, 2, 'dispose count (2/3)');
+	t.ok(disposed.has(map), 'dispose roughnessMap, metalnessMap');
+
+	textureDef.dispose();
+	documentView.gc();
+
+	t.equals(disposed.size, 3, 'dispose count (3/3)');
+	t.ok(disposed.has(texture), 'dispose roughnessMap, metalnessMap');
+
 	t.end();
 });
