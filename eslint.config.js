bec45668c346450988a60c6e26ac820dbeb8ae24
Don McCurdy <dm@donmccurdy.com>
2024-04-29 18:14:00 -0400

    Upgrade dependencies

---
+++ eslint.config.js
@@ -0,0 +1,44 @@
+// @ts-check
+import eslint from "@eslint/js";
+import prettier from "eslint-config-prettier";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  prettier,
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": ["off"], // TODO
+      "@typescript-eslint/no-non-null-assertion": ["off"],
+      "@typescript-eslint/no-this-alias": ["off"],
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        { argsIgnorePattern: "^_" },
+      ],
+      "@typescript-eslint/no-use-before-define": "off",
+      "@typescript-eslint/no-var-requires": ["off"],
+      "array-bracket-spacing": ["warn"],
+      "comma-spacing": ["warn"],
+      "max-len": [
+        "warn",
+        {
+          code: 120,
+          tabWidth: 4,
+          ignoreUrls: true,
+          ignorePattern: "^import|^export",
+        },
+      ],
+      "no-irregular-whitespace": ["warn"],
+      "space-infix-ops": ["warn"],
+      eqeqeq: ["warn", "smart"],
+      semi: ["error"],
+    },
+  },
+  {
+    files: ["test/**/*.ts"],
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+    },
+  },
+);
